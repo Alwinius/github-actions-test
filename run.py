a1f8d25931d96f3f3978e29c0e138a1ad3ba14b9
Alwin <github@alwin.net.au>
2019-11-14 20:29:34 +0100

    first try github action with packages

---
+++ run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# created by Alwin Ebermann (alwin@alwin.net.au)
+
+from time import sleep
+
+print("Hello World")
+sleep(50000)
+print("Hello again")
